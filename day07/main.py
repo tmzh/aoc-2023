bbca8a66a5ead76078060f3a9e730e727e9360c7
tmzh <2162408+tmzh@users.noreply.github.com>
2023-12-16 19:18:17 +0800

    day 07

--- day07/main.py
+++ day07/main.py
@@ -4,14 +4,10 @@ from enum import IntEnum
 from utils import parse_challenge
 
 
-def cmp(hand_1, hand_2):
+def cmp(hand_1, hand_2, strengths):
     for i in range(5):
         if hand_1[i] != hand_2[i]:
             return strengths.index(hand_1[i]) > strengths.index(hand_2[i])
-
-
-strengths = 'AKQJT98765432'[::-1]
-types = '543210'
 
 
 class HandType(IntEnum):
@@ -25,48 +21,65 @@ class HandType(IntEnum):
 
 
 class Hand:
-    def __init__(self, hand, bid=0):
+    strengths = 'AKQJT98765432'[::-1]
+    strengths_2 = 'AKQT98765432J'[::-1]
+
+    def __init__(self, hand, bid=0, part_2=False):
         self.hand = hand
         self.bid = bid
+        self.part_2 = part_2
 
     def __lt__(self, other):
         if self.hand_type < other.hand_type:
             return True
         elif self.hand_type == other.hand_type:
-            return cmp(other.hand, self.hand)
+            return cmp(other.hand, self.hand, self.strengths_2 if self.part_2 else self.strengths)
         return False
 
     @property
     def hand_type(self):
-        counts = sorted(Counter(self.hand).values(), reverse=True)
-        if counts[0] == 5:
-            return HandType.FIVE_OF_A_KIND
-        if counts[0] == 4:
-            return HandType.FOUR_OF_A_KIND
-        if counts[0] == 3:
-            if counts[1] == 2:
+        hand = self.hand
+        if self.part_2:
+            if 'J' in self.hand:
+                c = Counter(hand)
+                if c.most_common()[0][0] == 'J':
+                    if len(c) > 1:
+                        top_card = c.most_common(2)[1][0]
+                    else:
+                        top_card = 'J'
+                else:
+                    top_card = c.most_common(1)[0][0]
+                hand = self.hand.replace('J', top_card)
+
+        match sorted(Counter(hand).values(), reverse=True):
+            case [5]:
+                return HandType.FIVE_OF_A_KIND
+            case [4, 1]:
+                return HandType.FOUR_OF_A_KIND
+            case [3, 2]:
                 return HandType.FULL_HOUSE
-            else:
+            case [3, 1, 1]:
                 return HandType.THREE_OF_A_KIND
-        if counts[0] == 2:
-            if counts[1] == 2:
+            case [2, 2, 1]:
                 return HandType.TWO_PAIR
-            else:
+            case [2, 1, 1, 1]:
                 return HandType.ONE_PAIR
-        return HandType.HIGH_CARD
+            case _:
+                return HandType.HIGH_CARD
 
     def __repr__(self):
         return f'Hand({self.hand}, {self.bid})'
 
 
-def score(challenge):
-    hands = [Hand(h, s) for h, s in map(str.split, challenge)]
+def score(challenge, part_2=False):
+    hands = [Hand(h, s, part_2) for h, s in map(str.split, challenge)]
     winnings = 0
     for i, h in enumerate(sorted(hands)):
-        winnings += (1+i) * int(h.bid)
+        winnings += (1 + i) * int(h.bid)
     return winnings
 
 
 if __name__ == '__main__':
     challenge = parse_challenge(7)
     print("Part 1: ", score(challenge))
+    print("Part 2: ", score(challenge, part_2=True))
--- day07/test.py
+++ day07/test.py
@@ -14,11 +14,26 @@ QQQJA 483'''
     def test_hand_cmp(self):
         self.assertGreater(Hand('QQQJA', None), Hand('T55J5', None))
 
+    def test_hand_cmp(self):
+        self.assertGreater(Hand('2QQQQ', None, part_2=True), Hand('JKKK2', None, part_2=True))
+
     def test_hand_type(self):
-        self.assertEqual(Hand('QQQJA').hand_type, HandType.THREE_OF_A_KIND )
+        self.assertEqual(Hand('QQQJA').hand_type, HandType.THREE_OF_A_KIND)
         self.assertEqual(Hand('KK677').hand_type, HandType.TWO_PAIR)
+
+    def test_hand_type_2(self):
+        self.assertEqual(Hand('QJJQ2', part_2=True).hand_type, HandType.FOUR_OF_A_KIND)
+        self.assertEqual(Hand('T55J5', part_2=True).hand_type, HandType.FOUR_OF_A_KIND)
+        self.assertEqual(Hand('KTJJT', part_2=True).hand_type, HandType.FOUR_OF_A_KIND)
+        self.assertEqual(Hand('QQQJA', part_2=True).hand_type, HandType.FOUR_OF_A_KIND)
+        self.assertEqual(Hand('KK677', part_2=True).hand_type, HandType.TWO_PAIR)
+        self.assertEqual(Hand('JJJJ2', part_2=True).hand_type, HandType.FIVE_OF_A_KIND)
+        self.assertEqual(Hand('JJJJJ', part_2=True).hand_type, HandType.FIVE_OF_A_KIND)
 
     def test_score(self):
         lines = parse_challenge(self.sample_input)
         self.assertEqual(score(lines), 6440)
 
+    def test_score_2(self):
+        lines = parse_challenge(self.sample_input)
+        self.assertEqual(score(lines, True), 5905)
